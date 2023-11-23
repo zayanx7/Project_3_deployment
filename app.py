201420e8c98a696a6ca621eb45e6ad1ba66481f8
zayanx7 <zayan1579@gmail.com>
2023-11-23 03:49:09 +0000

    dep

--- app.py
+++ app.py
@@ -110,4 +110,4 @@ def map_visualization():
     return render_template('map.html', location_data=location_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port = 8000, debug=True)
